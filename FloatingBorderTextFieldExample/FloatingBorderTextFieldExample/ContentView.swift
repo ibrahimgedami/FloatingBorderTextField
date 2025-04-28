32d13d379b9b5bddccf5a76028265b2a1f331f78
ibrahimgedami <ibrahim.abdelgani@seddiqiholding.com>
2025-04-28 08:02:23 +0400

    commit latest changes

--- FloatingBorderTextFieldExample/FloatingBorderTextFieldExample/ContentView.swift
+++ FloatingBorderTextFieldExample/FloatingBorderTextFieldExample/ContentView.swift
@@ -13,21 +13,34 @@ struct ContentView: View {
     @State private var firstName: String = ""
     @State private var lastName: String = ""
     
+    @State private var email = ""
+    
     private let columns = [
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
     
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 20) {
-            FloatingBorderTextField(title: "First Name", text: $firstName)
-            FloatingBorderTextField(title: "Last Name", text: $lastName)
+        VStack(spacing: 20) {
+            LazyVGrid(columns: columns, spacing: 20) {
+                FloatingBorderTextField(title: "First Name", text: $firstName)
+                FloatingBorderTextField(title: "Last Name", text: $lastName)
+            }
+            .padding()
         }
-        .padding()
     }
-    
+
 }
 
 #Preview {
     ContentView()
+}
+
+extension String {
+    
+    var isValidEmail: Bool {
+        let emailRegEx = #"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
+        return NSPredicate(format: "SELF MATCHES %@", emailRegEx).evaluate(with: self)
+    }
+
 }
